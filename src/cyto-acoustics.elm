0aec48071b2e32eb59a6091e6d577da4cb33857a
makemeunsee <makemeunsee@gmail.com>
2016-05-22 14:22:42 +0200

    preparing mouse modulation

--- src/cyto-acoustics.elm
+++ src/cyto-acoustics.elm
@@ -7,8 +7,9 @@ import Html.Events exposing (onClick, onMouseEnter)
 import Debug exposing (log)
 import Time exposing (every, millisecond)
 import Task
-import Mouse
+import Mouse exposing (Position)
 import Array exposing (Array)
+import Window exposing (Size)
 
 import Matrix
 
@@ -24,15 +25,33 @@ main =
 
 -- MODEL
 
-type alias Model = { matrix: Matrix.Matrix, clicked: Bool, live: Bool, mode: String }
+type alias NormedMousePosition =
+    { x : Float
+    , y : Float
+    }
+
+
+type alias Model =
+  { matrix: Matrix.Matrix
+  , clicked: Bool
+  , live: Bool
+  , mode: String
+  , screenSize: Size
+  , mousePosition: Position
+  }
 
 
 init : Int -> (Model, Cmd Msg)
 init size =
-  (Model (Matrix.init size) False False "Full", getCurrentSeconds )
+  (Model (Matrix.init size) False False "Full" (Size 0 0) (Position 0 0), Cmd.batch [getCurrentSeconds, getCurrentScreenSize] )
 
 
-getCurrentSeconds = Task.perform (\_ -> Clear) (\time -> CurrentSeconds (floor (time / 1000) ) ) Time.now
+getCurrentSeconds =
+  Task.perform (\_ -> Clear) (\time -> CurrentSeconds (floor (time / 1000) ) ) Time.now
+
+
+getCurrentScreenSize =
+  Task.perform (\_ -> ScreenResize (Size 1 1)) ScreenResize Window.size
 
 
 -- UPDATE
@@ -47,6 +66,8 @@ type Msg =
   | MatMsg Matrix.Msg
   | Ship (String)
   | SwitchMode (String)
+  | MouseMove Position
+  | ScreenResize Size
   --| MickeyDown
   --| MickeyUp
   --| DragMsg Switch
@@ -67,6 +88,8 @@ update msg model =
         ( { model | matrix = newMatrix }, newCells changes )
     Ship kind -> (model, Cmd.none) --TODO switch ship kind
     SwitchMode newMode ->  ( { model | mode = newMode }, Cmd.none )
+    MouseMove position -> ( { model | mousePosition = position }, Cmd.none )
+    ScreenResize size -> ( { model | screenSize = size }, Cmd.none )
     --DragMsg sw ->
     --  if model.clicked then
     --    updateHelper sw model
@@ -74,6 +97,15 @@ update msg model =
     --    (model, Cmd.none)
     --MickeyDown -> ( { model | clicked = True }, Cmd.none)
     --MickeyUp -> ( { model | clicked = False }, Cmd.none)
+
+
+normed : Position -> Size -> NormedMousePosition
+normed {x, y} {width, height} =
+  let
+    xNormed = (toFloat x) / (toFloat width)
+    yNormed = (toFloat (height - y)) / (toFloat height)
+  in
+    NormedMousePosition xNormed yNormed
 
 
 port newCells : List (Int, Int) -> Cmd msg
@@ -92,17 +124,19 @@ port switchMode : (String -> msg) -> Sub msg
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.batch [
-    --Mouse.downs (always MickeyDown),
-    --Mouse.ups (always MickeyUp),
-    randomize (always Randomize),
-    clear (always Clear),
-    nextStep (always (MatMsg Matrix.NextGeneration)),
-    every (150*millisecond) (always Tick),
-    toggleLive (always ToggleLive),
-    ship Ship,
-    switchMode SwitchMode
-  ]
+  Sub.batch
+    [ randomize (always Randomize)
+    , clear (always Clear)
+    , nextStep (always (MatMsg Matrix.NextGeneration))
+    , every (150*millisecond) (always Tick)
+    , toggleLive (always ToggleLive)
+    , ship Ship
+    , switchMode SwitchMode
+    , Mouse.moves MouseMove
+    , Window.resizes ScreenResize
+    --, Mouse.downs (always MickeyDown)
+    --, Mouse.ups (always MickeyUp)
+    ]
 
 
 -- VIEW
@@ -110,7 +144,8 @@ subscriptions model =
 
 viewCell : Int -> Int -> Bool -> Html Msg
 viewCell row col cell =
-  td [ class (if cell then "on" else "off")
+  td
+    [ class (if cell then "on" else "off")
     , onClick (MatMsg (Matrix.Toggle row col))
     --, onMouseEnter (DragMsg msg)
     ]
